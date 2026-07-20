f08dad665550a4ca7e4ee5f26ec0ac67bc24079c
Time-Of-Creat <matkago12345new@gmail.com>
2026-07-20 15:30:26 +0300

    Added login and signup pages

--- Habit/urls.py
+++ Habit/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.habits_list, name='habits_list')
+    path('', views.habits_list, name='habits_list'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- Habit/views.py
+++ Habit/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
 
 
 @login_required
