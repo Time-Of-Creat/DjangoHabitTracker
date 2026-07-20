538562f497562578a1cc5ac3c3d27d6fe2556545
Time-Of-Creat <matkago12345new@gmail.com>
2026-07-20 15:40:32 +0300

    created Habit model

--- Habit/models.py
+++ Habit/models.py
@@ -2,4 +2,21 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
-user = get_user_model()
+User = get_user_model()
+
+
+class Habit(models.Model):
+    title = models.CharField(max_length=200)
+    complete_today = models.BooleanField(default=False)
+    streak = models.IntegerField(default=0)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def complete(self):
+        self.complete_today = True
+        self.streak += 1
+        self.save()
