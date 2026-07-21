48cdf5d43560f57a19df30c5b48b65d72c065ced
Time-Of-Creat <matkago12345new@gmail.com>
2026-07-21 19:42:46 +0300

    Fixed bugs

--- HabitTracker/celery.py
+++ HabitTracker/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoHabitTracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HabitTracker.settings')
 
-app = Celery('DjangoHabitTracker')
+app = Celery('HabitTracker')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
